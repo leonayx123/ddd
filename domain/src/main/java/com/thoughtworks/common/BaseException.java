7319aaf1d203d5636538d666f7ee10093d2987e8
yx <leonayx123@126.com>
2019-10-07 20:03:39 +0800

    exception handle

---
+++ domain/src/main/java/com/thoughtworks/common/BaseException.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.common;
+
+public class BaseException extends RuntimeException {
+
+
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
